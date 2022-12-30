ad475ff60917042609583b8e8efa17bda645c314
Serge Skop <sergeskop@yahoo.com>
2022-12-30 08:12:12 -0700

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -16,17 +17,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+    <div className="">
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>
-            { activity.title}
-          </li>
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
         ))}
-      </ul>
-      </header>
+      </List>
     </div>
   );
 }
